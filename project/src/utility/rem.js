fb45593e518d777e05d10e4307f44ce10ec502ec
boa182 <baoyi.zhen@partner.midea.com>
2018-05-17 14:31:25 +0800

    适配rem

---
+++ project/src/utility/rem.js
@@ -0,0 +1,15 @@
+/*
+  通过js动态设置根元素的rem
+*/
+(function (doc, win) {
+  var docEl = doc.documentElement
+  var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
+  var recalc = function () {
+    var clientWidth = docEl.clientWidth
+    if (!clientWidth) return
+    docEl.style.fontSize = 100 * (clientWidth / 375) + 'px'
+  }
+  if (!doc.addEventListener) return
+  win.addEventListener(resizeEvt, recalc, false)
+  doc.addEventListener('DOMContentLoaded', recalc, false)
+})(document, window)
